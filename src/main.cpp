da9717e2b49bbbb12593d3e39114aa9cbad2c381
Simone <simonefarinelli778@gmail.com>
2022-09-30 18:53:21 +0100

    All set, lets start

--- src/main.cpp
+++ src/main.cpp
@@ -6,17 +6,6 @@
 
 int main()
 {
-    std::cout << "Hello, i am code"
+    std::cout << "Hello world"
               << std::endl;
-
-    int age;
-    std::cin >> age;
-
-    std::cout << age << "\n";
-
-    return 0;
-
-    //this is a test, this should appear on github via my school account
-
-    //While this, should appear on github via my personal account
 }
