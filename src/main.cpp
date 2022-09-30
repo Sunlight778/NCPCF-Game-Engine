4e3cef88d64ce1e372636503243b83ff4cfa6a71
Simone <simonefarinelli778@gmail.com>
2022-09-30 18:31:40 +0100

    Code compiles

---
+++ src/main.cpp
@@ -0,0 +1,18 @@
+//
+// Created by Simon on 30/09/2022.
+//
+
+#include "iostream"
+
+int main()
+{
+    std::cout << "Hello, i am code"
+              << std::endl;
+
+    int age;
+    std::cin >> age;
+
+    std::cout << age << "\n";
+
+    return 0;
+}
