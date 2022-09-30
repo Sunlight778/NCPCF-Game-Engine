7eefdc508d8f642388747e0e9bb0462ff97d0f1f
Simone <simonefarinelli778@gmail.com>
2022-09-30 18:58:38 +0100

    Changed location

---
+++ cmake-build-debug/CMakeFiles/NCPCF_Game_Engine.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/NCPCF_Game_Engine.dir/src/main.cpp.obj"
+  "CMakeFiles/NCPCF_Game_Engine.dir/src/main.cpp.obj.d"
+  "NCPCF_Game_Engine.exe"
+  "NCPCF_Game_Engine.exe.manifest"
+  "NCPCF_Game_Engine.pdb"
+  "libNCPCF_Game_Engine.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/NCPCF_Game_Engine.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/NCPCF_Game_Engine.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for NCPCF_Game_Engine.
+# This may be replaced when dependencies are built.
